2465a39f8932fc28ca211edb8903fa69bc165a63
Aleksander Lorenc <aleksander.lorenc@olx.pl>
2020-01-18 16:11:04 +0100

    Prepares for PyPI distribution.

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
    url='https://github.com/evilmint/aws-openapi-lint',
    description='AWS API Gateway OpenAPI spec linter.',
    author='Aleksander Lorenc',
+   license = 'MIT',
    author_email='lorencaleksander@gmail.com',
    packages=find_packages(),
    include_package_data=True,
@@ -14,5 +15,15 @@ setup(
         'console_scripts': [
             'aws-openapi-lint=aws_openapi_lint:cli'
         ]
-    }
+   },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
