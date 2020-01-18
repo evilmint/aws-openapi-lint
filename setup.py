0a17e857b46757c2598d7d1eb987aac2d27d75cd
Aleksander Lorenc <aleksander.lorenc@olx.pl>
2020-01-18 17:10:34 +0100

    Updates classifiers & python requries.

--- setup.py
+++ setup.py
@@ -29,9 +29,7 @@ setup(
       'Intended Audience :: Developers',
       'Topic :: Software Development :: Build Tools',
       'License :: OSI Approved :: MIT License',
-      'Programming Language :: Python :: 3.4',
-      'Programming Language :: Python :: 3.5',
-      'Programming Language :: Python :: 3.6',
-      'Programming Language :: Python :: 3.7',
+      'Programming Language :: Python :: 3'
    ],
+   python_requires='>=3.6'
 )
